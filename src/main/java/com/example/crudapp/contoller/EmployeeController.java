efd4fdf55cfb50fe48e988f3baa736cd898cbe3e
abdelhak4 <teqaniabde@gamil.com>
2024-03-11 17:38:43 +0000

    init

---
+++ src/main/java/com/example/crudapp/contoller/EmployeeController.java
@@ -0,0 +1,21 @@
+package com.example.crudapp.contoller;
+
+import com.example.crudapp.model.Employee;
+import com.example.crudapp.repository.EmployeeRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+    private EmployeeRepository employeeRepository;
+
+    // get all employees
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
